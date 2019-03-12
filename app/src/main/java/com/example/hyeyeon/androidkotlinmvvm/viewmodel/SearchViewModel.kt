7499bebbbd0cf98be1be14d744c2c9075bc02af8
Hyeyeon.Park <gpdus2371@gmail.com>
2019-03-12 18:01:05 +0900

    update ui

--- app/src/main/java/com/example/hyeyeon/androidkotlinmvvm/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/example/hyeyeon/androidkotlinmvvm/viewmodel/SearchViewModel.kt
@@ -3,6 +3,8 @@ package com.example.hyeyeon.androidkotlinmvvm.viewmodel
 import android.arch.lifecycle.MutableLiveData
 import android.databinding.Bindable
 import android.databinding.ObservableField
+import android.databinding.ObservableInt
+import android.view.View
 import com.example.hyeyeon.androidkotlinmvvm.R
 import com.example.hyeyeon.androidkotlinmvvm.common.ResourceProvider
 import com.example.hyeyeon.androidkotlinmvvm.data.handler.SearchEventHandler
@@ -19,7 +21,7 @@ class SearchViewModel(private val handler: SearchEventHandler, private val repos
     private val logger = Logger.getLogger(this::class.java.name)!!
     private val NAVER_CLIENT_ID: String = resourceProvider.getString(R.string.naver_client_id)
     private val NAVER_CLIENT_SECRET: String = resourceProvider.getString(R.string.naver_client_secret)
-    private var display = 5
+    private var display = 15
 
     var page = 0
         set(value) {
@@ -32,17 +34,20 @@ class SearchViewModel(private val handler: SearchEventHandler, private val repos
         get() = field
 
     var query: String = ""
-        set(value) {
-            field = value
-            page = 0
-
-            searchResultList.postValue(null)
-            if (value.isNotBlank()) getSearchResults()
-        }
 
     var searchResultList = MutableLiveData<List<SearchResponseItem>>().apply { ArrayList<SearchResponseItem>() }
 
+    var historyVisibility = ObservableInt(View.GONE)
+        @Bindable
+        get() = field
+
     fun onClickItem(message: String) = handler.onClickItem(message)
+
+    fun onClickSearch() {
+        page = 0
+        searchResultList.postValue(null)
+        getSearchResults()
+    }
 
     fun getSearchResults() {
         GlobalScope.launch(Dispatchers.Default, CoroutineStart.DEFAULT, null, {
@@ -57,4 +62,15 @@ class SearchViewModel(private val handler: SearchEventHandler, private val repos
             }
         })
     }
+
+    fun onClickHistory() {
+        historyVisibility = if (historyVisibility.get() == View.VISIBLE) {
+            ObservableInt(View.GONE)
+        } else {
+            ObservableInt(View.VISIBLE)
+        }
+
+        historyVisibility.notifyChange()
+    }
+
 }
